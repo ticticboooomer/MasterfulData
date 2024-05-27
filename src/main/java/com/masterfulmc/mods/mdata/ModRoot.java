0d8c839e101c1caefb2733e23337df9709ec55b4
ticticboooomer <kylebc-dev@outlook.com>
2024-05-27 22:15:34 +0100

    init

---
+++ src/main/java/com/masterfulmc/mods/mdata/ModRoot.java
@@ -0,0 +1,10 @@
+package com.masterfulmc.mods.mdata;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Ref.ID)
+public class ModRoot {
+    public ModRoot() {
+
+    }
+}
---
+++ src/main/java/com/masterfulmc/mods/mdata/Ref.java
@@ -0,0 +1,5 @@
+package com.masterfulmc.mods.mdata;
+
+public class Ref {
+    public static final String ID = "mdata";
+}
